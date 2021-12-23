8e2bcd41c4b451744c38513c2f43e28a37d9fda9
Obez99 <giuliogx@live.it>
2021-12-23 14:45:28 +0100

    Styles cards

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <div class="container">
-      <div class="row row-cols-5 mt-5">
+      <div class="row row-cols-5 mt-5"> 
         @foreach ($movies as $movie)
         <div class="col mt-5">    
-          <div class="card">
+          <div class="card h-100 text-white bg-dark">
             <div class="card-body">
-              <h5 class="card-title pb-3">{{$movie->title}}</h5>
+              <h5 class="card-title text-danger pb-3">{{$movie->title}}</h5>
               <p class="card-text"><strong>Original title</strong>: {{$movie->original_title}}</p>
               <p class="card-text"><strong>Nationality</strong>: {{$movie->nationality}}</p>
               <p class="card-text"><strong>Date</strong>: {{$movie->date}}</p>
